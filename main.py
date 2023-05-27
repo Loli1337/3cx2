37cbb36a95762a3e481c99b1c40e909ea7ec64a9
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-27 06:03:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from requests import post
 from sys import argv
-if {argv[1]} == '1001':
+if argv[1] == '1001':
     telegram_id = '5280692596'
-elif {argv[1]} == '1002':
+elif argv[1] == '1002':
     telegram_id = '1197549044'
-elif {argv[1]} == '1003':
+elif argv[1] == '1003':
     telegram_id = '685382438'
-elif {argv[1]} == '1004':
+elif argv[1] == '1004':
     telegram_id = '5368332363'
-elif {argv[1]} == '9998':
+elif argv[1] == '9998':
     telegram_id = '1907961854'
 post(f'https://api.telegram.org/bot5113940874:AAFDT9PN52fIlKbbOWmtF9DF503sde-9P0M/sendMessage?chat_id=-1001549929537&text={telegram_id}\n{argv[2]}')
